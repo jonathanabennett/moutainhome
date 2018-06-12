06fa149c8e1fd40f874631e672c05f6420936839
Jonathan A. Bennett <doulos05@gmail.com>
2018-06-12 17:03:05 +0900

    Hello World program.

--- display.nim
+++ display.nim
@@ -1,3 +1,10 @@
-#import nimbox
+import nimbox
+import os
 
-echo "Testing"
+when isMainModule:
+  var nb = newNimbox()
+  defer: nb.shutdown()
+
+  nb.print(0, 0, "Hello, world!")
+  nb.present()
+  sleep(1000)
