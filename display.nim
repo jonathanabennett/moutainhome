70dc90b04997b6decfca10b9e5d4cf37fb67e49d
Jonathan A. Bennett <doulos05@gmail.com>
2018-06-12 18:03:26 +0900

    Add Loop until Escape.

--- display.nim
+++ display.nim
@@ -1,10 +1,23 @@
 import nimbox
 import os
 
+proc hello(screen: Nimbox): bool =
+  var evt: Event
+
+  while true:
+    screen.clear()
+    screen.print(0, 0, "Hello, world!")
+    screen.present()
+
+    evt = screen.peekEvent(1000)
+    case evt.kind:
+      of EventType.Key:
+        if evt.sym == Symbol.Escape:
+          break
+      else: discard
+
 when isMainModule:
   var nb = newNimbox()
   defer: nb.shutdown()
 
-  nb.print(0, 0, "Hello, world!")
-  nb.present()
-  sleep(1000)
+  discard hello(nb)
