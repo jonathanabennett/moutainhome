88d9a9196b3ba042baa5742b355d0fc11188c6c6
Jonathan A. Bennett <doulos05@gmail.com>
2018-06-12 18:23:29 +0900

    Centered display.

--- display.nim
+++ display.nim
@@ -3,10 +3,12 @@ import os
 
 proc hello(screen: Nimbox): bool =
   var evt: Event
+  var width: int = screen.width div 2
+  var height: int = screen.height div 2
 
   while true:
     screen.clear()
-    screen.print(0, 0, "Hello, world!")
+    screen.print(width, height, "Welcome to MountainHome.")
     screen.present()
 
     evt = screen.peekEvent(1000)
