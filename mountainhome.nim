e7d07010f96d7f4b0efc4d1d9950ad5779802dcc
Jonathan A. Bennett <doulos05@gmail.com>
2018-07-10 13:54:47 +0900

    Initial player diplay.

--- mountainhome.nim
+++ mountainhome.nim
@@ -1,19 +1,24 @@
 import nimbox
 import os
+import player
 
 proc hello(screen: Nimbox): bool =
   var evt: Event
   var width: int = screen.width
   var height: int = screen.height
+  var player: Player = newPlayer(0, width div 2, height div 2)
 
   while true:
 
     screen.clear()
     for x in 0..<width:
       for y in 0..<height:
-        screen.print(x, y, "#")
+        if x == 0 or y == 0 or x == width-1 or y == height-1:
+          screen.print(x, y, "#")
+        else: screen.print(x, y, ".")
 
-    screen.print(width, height, "Welcome to MountainHome.")
+    screen.print(player.x, player.y, "@")
+
     screen.present()
 
     evt = screen.peekEvent(1000)
---
+++ player.nim
@@ -0,0 +1,44 @@
+#import map
+#Player data
+
+type
+  Player* = ref object of RootObj
+    id*: int
+    x*: int
+    y*: int
+
+  Direction = enum
+    NORTH
+    NORTHEAST
+    EAST
+    SOUTHEAST
+    SOUTH
+    SOUTHWEST
+    WEST
+    NORTHWEST
+
+proc newPlayer*(id:int, x:int, y:int): Player =
+  Player(id:id, x:x, y:y)
+
+proc movePlayer*(player:Player, dir:Direction): bool =
+  case dir
+  of NORTH:
+    player.y -= 1
+  of NORTHEAST:
+    player.y -= 1
+    player.x += 1
+  of EAST:
+    player.x += 1
+  of SOUTHEAST:
+    player.x += 1
+    player.y += 1
+  of SOUTH:
+    player.y += 1
+  of SOUTHWEST:
+    player.x -= 1
+    player.y += 1
+  of WEST:
+    player.x -= 1
+  of NORTHWEST:
+    player.x -= 1
+    player.y -= 1
